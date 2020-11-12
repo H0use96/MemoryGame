1a610f8c10b5ceefc0cc996636fc1819e71d0b66
Taewoo Kang <taewookang96@gmail.com>
2020-11-12 17:48:59 +0900

    Initial Commit

---
+++ Memory_Game/Memory_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_GameApp.swift
+//  Memory_Game
+//
+//  Created by KHU_Taewoo on 2020/11/12.
+//
+
+import SwiftUI
+
+@main
+struct Memory_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
